ea851a12e84764f82551d2c2c0296780a6fda6af
danailivanov <[danailivanov94@gmail.com]>
2018-08-16 17:30:39 +0300

    create Rola and User Entity

---
+++ src/main/java/com/telerik/payment_system/entity/Role.java
@@ -0,0 +1,66 @@
+package com.telerik.payment_system.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+
+    public Role() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
---
+++ src/main/java/com/telerik/payment_system/entity/User.java
@@ -0,0 +1,97 @@
+package com.telerik.payment_system.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "EIK")
+    private String EIK;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+
+
+    public User() {
+        this.roles = new ArrayList<>();
+    }
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getEIK() {
+        return EIK;
+    }
+
+    public void setEIK(String EIK) {
+        this.EIK = EIK;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
