6ef12207729f5a44973dd084b44c24facbbd68cc
danailivanov <[danailivanov94@gmail.com]>
2018-08-16 18:17:17 +0300

    corrections

--- src/main/java/com/telerik/payment_system/config/SecurityConfiguration.java
+++ src/main/java/com/telerik/payment_system/config/SecurityConfiguration.java
@@ -1,13 +1,12 @@
 package com.telerik.payment_system.config;
 
-import com.telerik.payment_system.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -15,15 +14,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public SecurityConfiguration(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userService = userService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
