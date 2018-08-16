c3623f62f6c6f16919bd6d04896f8f5c9d1999e7
Kristina <k.bonevska@yahoo.com>
2018-08-16 15:55:49 +0300

    add securityConfig add aplicationBeanConfig

--- src/main/java/com/telerik/payment_system/config/SecurityConfiguration.java
+++ src/main/java/com/telerik/payment_system/config/SecurityConfiguration.java
@@ -1,4 +1,49 @@
 package com.telerik.payment_system.config;
 
-public class SecurityConfiguration {
+import com.telerik.payment_system.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SecurityConfiguration(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/register","/bootstrap/**", "/jquery/**", "/css/**", "/fonts/**", "/js/**", "/img/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login?logout").logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/unauthorized")
+                .and()
+                .csrf().disable();
+
+    }
+
+
+
 }
