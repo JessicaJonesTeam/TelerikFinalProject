0811f6f70fc4963416a66c435bc28eaa158206bb
Kristina <k.bonevska@yahoo.com>
2018-08-26 20:55:16 +0300

    add UserViewModel add UserBindingModel add BillRecordBindingModel add SubscriberBindingModel

--- src/main/java/com/telerik/payment_system/config/ApplicationBeanConfig.java
+++ src/main/java/com/telerik/payment_system/config/ApplicationBeanConfig.java
@@ -1,6 +1,8 @@
 package com.telerik.payment_system.config;
 
 
+import com.telerik.payment_system.Utilities.JwtParser;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,5 +15,13 @@ public class ApplicationBeanConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public JwtParser jwtParser() {
+        return new JwtParser();
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
 }
