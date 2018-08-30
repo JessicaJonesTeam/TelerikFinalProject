5fd40fe4449d0e93352519227cb0c9080d4218ab
Kristina <k.bonevska@yahoo.com>
2018-08-30 17:37:52 +0300

    add role to header

--- src/main/java/com/telerik/payment_system/JWT/JwtAuthenticationFilter.java
+++ src/main/java/com/telerik/payment_system/JWT/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.telerik.payment_system.JWT;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.telerik.payment_system.constants.Constants;
+import com.telerik.payment_system.entities.Role;
 import com.telerik.payment_system.entities.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -41,7 +43,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 //                            todo: authorities or empy array list
                             new ArrayList<>())
             );
-        } catch(IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -50,17 +52,29 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request,
                                             HttpServletResponse response,
                                             FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
+                                            Authentication authResult) {
         String token = Jwts.builder()
                 .setSubject(((User) authResult.getPrincipal()).getUsername())
                 .setExpiration(new Date(System.currentTimeMillis() + Constants.EXPIRATION_DURATION))
                 .signWith(SignatureAlgorithm.HS256, Constants.SECRET.getBytes())
                 .compact();
 
-        response.getWriter().append("{\"Authorization\": \"Bearer " + token + "\"}");
+        System.out.println("Generated token - " + token);
         response.setContentType("application/json");
-//        response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("Authorization", "Bearer " + token);
 
+        String userRole = ((List<Role>) authResult.getAuthorities()).get(0).getAuthority();
+
+        try {
+            response.getWriter()
+                    .append("{\"Authorization\": \"Bearer ").append(token).append("\"")
+                    .append(", \"Role\": \"").append(userRole)
+                    .append("\"}\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        response.addHeader("Authorization", "Bearer " + token);
 
     }
 }
